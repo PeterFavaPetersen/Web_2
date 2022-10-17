2daad3ea438180242df4f5fed57b3e65eb0329bc
Pedro Petersen <PedroFavaPetersen@gmail.com>
2022-10-16 21:52:15 -0300

    Update db_sesion.sql

--- database/db_sesion.sql
+++ database/db_sesion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2022 a las 16:26:19
+-- Tiempo de generación: 17-10-2022 a las 02:49:29
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `campeones`
+--
+
+CREATE TABLE `campeones` (
+  `id_campeones` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id_juego` int(11) NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `campeones`
+--
+
+INSERT INTO `campeones` (`id_campeones`, `nombre`, `id_juego`, `duracion`, `fecha`) VALUES
+(29, 'Franco', 28, 30, '2022-10-15'),
+(33, 'Daniela', 27, 14, '2022-09-29'),
+(34, 'Julian', 28, 132, '2022-09-25'),
+(35, 'Camilo', 30, 587, '2022-09-21'),
+(36, 'Mariano', 26, 123, '2022-09-26'),
+(37, 'Tomas', 27, 1234, '2022-10-12'),
+(38, 'Nicolas', 28, 23, '2022-09-28'),
+(39, 'Celeste', 26, 234, '2022-10-05'),
+(40, 'Rocio', 28, 142, '2022-09-27'),
+(41, 'Sol', 30, 15, '0000-00-00'),
+(42, 'Camilo', 33, 78, '2022-09-28'),
+(43, 'Daniela', 34, 15, '2022-10-15');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mesadejuego`
 --
 
@@ -39,16 +71,46 @@ CREATE TABLE `mesadejuego` (
 --
 
 INSERT INTO `mesadejuego` (`id_mesadejuego`, `juego`, `director`, `cantidadJugadores`) VALUES
-(1, 'Monopoly', 'El Banco', 10),
-(26, 'Truco', 'no', 12),
+(26, 'Monopoly', 'El Banco', 90),
 (27, 'Catan', 'Ladron', 8),
 (28, 'La vida', 'La Suerte', 1),
 (30, 'Cobra', 'La Cobra', 4),
-(31, 'Uno', 'no', 8);
+(33, 'Chanco', 'no', 8),
+(34, 'Ajedrez', 'no', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `name_usuario` varchar(15) NOT NULL,
+  `password_usuario` varchar(255) NOT NULL,
+  `log_usuario` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `name_usuario`, `password_usuario`, `log_usuario`) VALUES
+(4, 'marianoadmin', '$2y$10$AP0cnhS7ZX/byGZJto2lg.OZ5wjwmfp0eMX8l.07lNp1LlBzbaNYu', NULL),
+(5, 'danielaadmin', '$2y$10$QOBVsT/XetdOMjqBWLZLHOpW9jJuh1Sy7nuCvmZaEDI4SmJWf5Fna', NULL),
+(6, 'PedroPetersen', '$2y$10$g./cqKD1l9JhaYC9c8gdb.Kp2uVtQd7axm4twXDLhmEbfpQPBezMu', 1),
+(7, 'PedroFava', '$2y$10$a7A//o7gu5oVgycqyJb9zOUwbU41H6hzXVC6U1Zwx7iBTnw2XnpG2', 1);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `campeones`
+--
+ALTER TABLE `campeones`
+  ADD PRIMARY KEY (`id_campeones`),
+  ADD KEY `id_juego` (`id_juego`);
 
 --
 -- Indices de la tabla `mesadejuego`
@@ -57,14 +119,42 @@ ALTER TABLE `mesadejuego`
   ADD PRIMARY KEY (`id_mesadejuego`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `campeones`
+--
+ALTER TABLE `campeones`
+  MODIFY `id_campeones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de la tabla `mesadejuego`
 --
 ALTER TABLE `mesadejuego`
-  MODIFY `id_mesadejuego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_mesadejuego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `campeones`
+--
+ALTER TABLE `campeones`
+  ADD CONSTRAINT `campeones_ibfk_1` FOREIGN KEY (`id_juego`) REFERENCES `mesadejuego` (`id_mesadejuego`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
